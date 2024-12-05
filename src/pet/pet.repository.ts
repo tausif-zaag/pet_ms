5340a8354cd81b804e8cb7237cf7e9fd4689c8c4
tausif-zaag <tausif@zaagsys.com>
2024-12-05 16:41:36 +0600

    Refactor ownerRepository

--- src/pet/pet.repository.ts
+++ src/pet/pet.repository.ts
@@ -11,15 +11,25 @@ export class PetRepository extends Repository<Pet> {
     async findById(id: number) {
         return await this.findOne({
             where: { id: id },
+            relations: ['owner'],
         });
     }
 
-    async findAll() {
-        return await this
-            .createQueryBuilder('pet')
-            .leftJoinAndSelect('pet.category', 'category')  // Join the category relation
-            .leftJoinAndSelect('pet.owner', 'owner')        // Join the owner relation
-            .leftJoinAndSelect('pet.store', 'store')        // Join the store relation
+    async findAll(
+        categoryId: number,
+        page: number = 1,
+        limit: number = 10,
+        isAdopted: boolean = false,
+    ): Promise<{ data: Pet[], totalCount: number, totalPages: number }> {
+        const maxLimit = 100; // Maximum items per page
+        limit = Math.min(Math.max(limit, 1), maxLimit);
+
+        console.debug('IsAdopted [petRepository]:', isAdopted);
+
+        const queryBuilder = this.createQueryBuilder('pet')
+            .leftJoinAndSelect('pet.category', 'category')
+            .leftJoinAndSelect('pet.owner', 'owner')
+            .leftJoinAndSelect('pet.store', 'store')
             .select([
                 'pet.id',
                 'pet.name',
@@ -28,15 +38,37 @@ export class PetRepository extends Repository<Pet> {
                 'pet.description',
                 'pet.weight',
                 'pet.color',
-                'pet.isAdopted',
-                'category.id',   // Select only the id of the category
-                'category.name', // Select only the name of the category
-                'owner.id',      // Select only the id of the owner
-                'owner.first_name', // Select only the first name of the owner
-                'store.id',      // Select only the id of the store
-                'store.name',    // Select only the name of the store
-            ])
-            .getMany();  // Executes the query and returns the results
+                'pet.is_adopted',
+                'category.id',
+                'category.name',
+                'owner.id',
+                'owner.first_name',
+                'store.id',
+                'store.name',
+            ]);
+
+        // Default filter for isAdopted is false, but this can be overridden by the provided isAdopted value
+        queryBuilder.where('pet.is_adopted = :isAdopted', { isAdopted });
+
+        if (categoryId) {
+            queryBuilder.where('pet.category_id = :categoryId', { categoryId });
+        }
+
+
+        // Fetch the data and total count
+        const [data, totalCount] = await queryBuilder
+            .skip((page - 1) * limit) // Calculate the number of items to skip
+            .take(limit)              // Limit the number of items per page
+            .getManyAndCount();       // Get the data and total count
+
+        // Calculate total pages based on totalCount and limit
+        const totalPages = Math.ceil(totalCount / limit);
+
+        return {
+            data,
+            totalCount,
+            totalPages,
+        };
     }
 
 }
