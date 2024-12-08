779e8950948038c6c82b0f4d34b1ebfc4467f265
tausif-zaag <tausif@zaagsys.com>
2024-12-08 16:49:46 +0600

    Commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { envConfig } from './constants/env.constant';
-import { createDatabaseIfNotExists, } from './config/database.config';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { createDatabaseIfNotExists } from './config/database.config';
 import { configureWebSettings } from './config/web.config';
+import { envConfig } from './constants/env.constant';
 
 async function bootstrap() {
     await createDatabaseIfNotExists();
@@ -13,6 +14,16 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe());
 
     configureWebSettings(app);
+
+    const config = new DocumentBuilder()
+        .setTitle('Pet Shop')
+        .setDescription('Pet Shop API description')
+        .setVersion('1.0')
+        .addTag('pet-shop')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+
 
     await app.listen(envConfig.SERVER_PORT ?? 3000);
 }
